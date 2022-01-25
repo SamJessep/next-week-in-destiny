431f3c17db02df9c1ec81c140f2631b06afd424f
sam <samjessep77@gmail.com>
2022-01-26 00:51:59 +1300

    added darker background for modals. closes #6

--- components/Modal.vue
+++ components/Modal.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="background" v-if="shown" @click="backgroundClicked">
+  <div :class="'background' + (open?' open':'')" v-if="shown" @click="backgroundClicked">
     <component :is="modalComponent.name" v-bind="modalComponent.props" :closeModal="HIDE_DIALOG"/>
   </div>
 </template>
@@ -10,12 +10,35 @@ import { mapMutations } from 'vuex';
 
 export default {
   props:["modalProps", "modalComponent", "shown"],
+  data(){
+    return {
+      open:false,
+      timeout:null
+    }
+  },
   methods:{
     ...mapMutations(["HIDE_DIALOG"]),
     backgroundClicked(e){
       if(e.target.className.includes("background"))this.HIDE_DIALOG()
     }
   },
+  watch: {
+    $props: {
+      handler() {
+        if(this.shown){
+          this.timeout = setTimeout(()=>{
+            console.log("a")
+            this.open=true
+          },50)
+        }else{
+          if(this.timeout)clearTimeout(this.timeout)
+          this.open = false
+        }
+      },
+      deep: true,
+      immediate: true,
+    },
+  }
 }
 </script>
 
@@ -26,8 +49,12 @@ export default {
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: #00000021;
+  transition: background-color 1s;
   z-index: 10;
   display: block;
+}
+
+.background.open{
+  background-color: rgb(0 0 0 / 60%);
 }
 </style>
